2159218fabb08c9e60577608155deab29344459a
ww_side <daniil.metelia@gmail.com>
2023-09-06 16:37:42 +0300

    feat(middleware): add middleware

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  if (req.method === 'OPTIONS') next();
+
+  try {
+    const authorizationHeader = req.header('Authorization');
+    if (!authorizationHeader) {
+      return res.status(403).json({ message: 'User not authorized' });
+    }
+
+    const token = authorizationHeader.split(' ')[1];
+    if (!token) {
+      return res.status(403).json({ message: 'User not authorized' });
+    }
+
+    const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
+    req.user = { userId: decodedToken.id };
+    next();
+  } catch (e) {
+    console.log(e);
+    return res.status(403).json({ message: 'User not authorized' });
+  }
+};
