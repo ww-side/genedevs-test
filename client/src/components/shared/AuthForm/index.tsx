01b2fb114b3995076c5ed6da4da7a5505f236ae8
ww_side <daniil.metelia@gmail.com>
2023-09-07 09:39:28 +0300

    feat(components): add AuthForm

---
+++ client/src/components/shared/AuthForm/index.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import RegistrationForm from './RegistrationForm';
+
+const AuthForm: FC = () => {
+  return (
+    <>
+      <RegistrationForm />
+    </>
+  );
+};
+
+export default AuthForm;
